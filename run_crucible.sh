5a7e711c3b64bdc188293d4ed3ebe3de2da5ea7b
Jay Kickliter <jay@kickliter.com>
2016-06-27 11:30:00 -0700

    Change test parameters

--- run_crucible.sh
+++ run_crucible.sh
@@ -2,11 +2,11 @@
 
 rm -rf crucible-*
 
-stack exec crucible -- tester                                            \
-  --build-cmd="stack exec caut-rust-ref -- --spec=%s --output=rust"      \
-  --build-cmd="cargo build --manifest-path rust/Cargo.toml"              \
-  --run-cmd="RUST_BACKTRACE=1 cargo run --manifest-path rust/Cargo.toml" \
-  --schema-count=10                                                      \
-  --instance-count=100                                                   \
-  --type-count=100                                                       \
-  --enc-size=1024                                                        \
+stack exec crucible -- tester                                                                \
+  --build-cmd="stack exec caut-rust-ref -- --spec=%s --output=rust"                          \
+  --build-cmd="cargo build --manifest-path rust/Cargo.toml"                                  \
+  --run-cmd="tee rust/data.bin | RUST_BACKTRACE=1 cargo run --manifest-path rust/Cargo.toml" \
+  --schema-count=1                                                                           \
+  --instance-count=1000                                                                      \
+  --type-count=100                                                                           \
+  --enc-size=524288                                                                          \
